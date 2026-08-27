873abaf801dd89fae00915e3bb703a7c0044ff10
hanwate-darshan <darshanmrh12@gmail.com>
2026-08-28 00:22:19 +0530

    new functionlity

--- backend/scratch/debug_bull.js
+++
@@ -1,4 +0,0 @@
-const { ExpressAdapter } = require('@bull-board/express');
-const serverAdapter = new ExpressAdapter();
-console.log('getRouter type:', typeof serverAdapter.getRouter);
-console.log('getRouter result type:', typeof serverAdapter.getRouter());
